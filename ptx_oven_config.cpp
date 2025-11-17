0ff353fb249ce9baec4153827bad3ae2c616eef2
thaidao <daonamthai@gmail.com>
2025-11-17 00:07:11 -0500

    add: getter and setter for all parameter in configuration

--- ptx_oven_config.cpp
+++ ptx_oven_config.cpp
@@ -42,6 +42,78 @@ void ptx_oven_reset_config_to_defaults(void) {
     pti_oven_config.iteration_period        = 50U;
 }
 
+/* Individual parameter setters */
+void ptx_oven_set_ignition_duration_ms(uint32_t duration_ms) {
+    pti_oven_config.ignition_duration_ms = duration_ms;
+}
+
+uint32_t ptx_oven_get_ignition_duration_ms(void) {
+    return pti_oven_config.ignition_duration_ms;
+}
+
+void ptx_oven_set_periodic_log_ms(uint32_t interval_ms) {
+    pti_oven_config.periodic_log_ms = interval_ms;
+}
+
+uint32_t ptx_oven_get_periodic_log_ms(void) {
+    return pti_oven_config.periodic_log_ms;
+}
+
+void ptx_oven_set_sensor_fault_window_ms(uint32_t window_ms) {
+    pti_oven_config.sensor_fault_window_ms = window_ms;
+}
+
+uint32_t ptx_oven_get_sensor_fault_window_ms(void) {
+    return pti_oven_config.sensor_fault_window_ms;
+}
+
+void ptx_oven_set_auto_resume_delay_ms(uint32_t delay_ms) {
+    pti_oven_config.auto_resume_delay_ms = delay_ms;
+}
+
+uint32_t ptx_oven_get_auto_resume_delay_ms(void) {
+    return pti_oven_config.auto_resume_delay_ms;
+}
+
+void ptx_oven_set_vref_range_v(float min_v, float max_v) {
+    pti_oven_config.vref_min_v = min_v;
+    pti_oven_config.vref_max_v = max_v;
+}
+
+float ptx_oven_get_vref_min_v(void) {
+    return pti_oven_config.vref_min_v;
+}
+
+float ptx_oven_get_vref_max_v(void) {
+    return pti_oven_config.vref_max_v;
+}
+
+void ptx_oven_set_temp_target_c(float target_c) {
+    pti_oven_config.temp_target_c = target_c;
+}
+
+float ptx_oven_get_temp_target_c(void) {
+    return pti_oven_config.temp_target_c;
+}
+
+void ptx_oven_set_temp_delta_c(float delta_c) {
+    pti_oven_config.temp_delta_c = delta_c;
+}
+
+float ptx_oven_get_temp_delta_c(void) {
+    return pti_oven_config.temp_delta_c;
+}
+
+void ptx_oven_set_max_ignition_attempts(uint8_t attempts) {
+    if (attempts > 0 && attempts <= 5) {
+        pti_oven_config.max_ignition_attempts = attempts;
+    }
+}
+
+uint8_t ptx_oven_get_max_ignition_attempts(void) {
+    return pti_oven_config.max_ignition_attempts;
+}
+
 uint16_t ptx_oven_get_iteration_period(void) {
     return pti_oven_config.iteration_period;
 }
--- ptx_oven_config.h
+++ ptx_oven_config.h
@@ -36,6 +36,42 @@ typedef struct {
     
 } ptx_oven_config_t;
 
+/**
+ * @brief Get pointer to current configuration (read-only access)
+ * @return Pointer to const configuration structure
+ */
+const ptx_oven_config_t* ptx_oven_get_config(void);
+
+/**
+ * @brief Update oven configuration with new parameters
+ * @param config Pointer to new configuration structure
+ * @note Changes take effect immediately on next control update
+ */
+void ptx_oven_set_config(const ptx_oven_config_t* config);
+
+/**
+ * @brief Reset configuration to default values
+ */
+void ptx_oven_reset_config_to_defaults(void);
+
+
+void ptx_oven_set_ignition_duration_ms(uint32_t duration_ms);
+uint32_t ptx_oven_get_ignition_duration_ms(void);
+void ptx_oven_set_periodic_log_ms(uint32_t interval_ms);
+uint32_t ptx_oven_get_periodic_log_ms(void);
+void ptx_oven_set_sensor_fault_window_ms(uint32_t window_ms);
+uint32_t ptx_oven_get_sensor_fault_window_ms(void);
+void ptx_oven_set_auto_resume_delay_ms(uint32_t delay_ms);
+uint32_t ptx_oven_get_auto_resume_delay_ms(void);
+void ptx_oven_set_vref_range_v(float min_v, float max_v);
+float ptx_oven_get_vref_min_v(void);
+float ptx_oven_get_vref_max_v(void);
+void ptx_oven_set_temp_target_c(float target_c);
+float ptx_oven_get_temp_target_c(void);
+void ptx_oven_set_temp_delta_c(float delta_c);
+float ptx_oven_get_temp_delta_c(void);
+void ptx_oven_set_max_ignition_attempts(uint8_t attempts);
+uint8_t ptx_oven_get_max_ignition_attempts(void);
 uint16_t ptx_oven_get_iteration_period(void);
 
 
